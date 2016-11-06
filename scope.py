80d71c275cd59b0678e809d5022efed23969c67b
nill-zhang <andy.ledway3@gmail.com>
2016-11-06 11:50:46 -0500

    add debug code

--- scope.py
+++ scope.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # by sfzhang @2016.11.06
+from pprint import pprint
 
 info = "global scope"
 number = 3
@@ -25,7 +26,34 @@ def first_level():
 
     second_level()
 
+x = 7
+y = 0
+
+
+def fun1():
+    y = 11
+    z = x + 1
+    print "fun1: x is %d, y is %d" % (x, y)
+    pprint(fun1.__globals__)
+    print fun1.__closure__
+
+    def fun2():
+        y = 22
+        z = 77
+        print "fun2: x is %d, y is %d" % (x, y)
+        pprint(fun2.__globals__)
+        print fun2.__closure__
+
+        def fun3():
+            y = 33
+            print "fun3: x is %d, y is %d, z is %d" % (x, y, z)
+            pprint(fun3.__globals__)
+            print fun3.__closure__
+        fun3()
+    fun2()
+
 
 if __name__ == "__main__":
     first_level()
     print info  # global info should be changed after calling first_level()
+    fun1()
