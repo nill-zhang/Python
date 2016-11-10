c7469639aaaac12c3743fdad2b6dfc10b785e20a
nill-zhang <andy.ledway3@gmail.com>
2016-11-09 20:52:04 -0500

    fix bugs

--- scope.py
+++ scope.py
@@ -35,20 +35,20 @@ def fun1():
     z = x + 1
     print "fun1: x is %d, y is %d" % (x, y)
     pprint(fun1.__globals__)
-    print fun1.__closure__
+    print repr(fun1.__closure__)
 
     def fun2():
         y = 22
         z = 77
         print "fun2: x is %d, y is %d" % (x, y)
         pprint(fun2.__globals__)
-        print fun2.__closure__
+        print repr(fun2.__closure__)
 
         def fun3():
             y = 33
             print "fun3: x is %d, y is %d, z is %d" % (x, y, z)
             pprint(fun3.__globals__)
-            print fun3.__closure__
+            print repr(fun3.__closure__)
         fun3()
     fun2()
 
