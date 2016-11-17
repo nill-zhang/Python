a5d28fe831f271f1c059d7103bf4b01f53d65af9
nill-zhang <andy.ledway3@gmail.com>
2016-11-16 23:56:42 -0500

    make adjustments

--- Walk一个目录进行遍历.py
+++ Walk一个目录进行遍历.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 #-*-coding = UTF-8-*-
 import os
-from os.path import join, getsize
+
 t = open('D:/python/python/oswalk.txt','w')
 for root, dirs, files in os.walk('D:\\work'):
-    t.write('--------------------------------------------------------------------------------------------------------------------------------\n')
+    t.write('-'*40+'\n')
     t.write('root:'+repr(root)+'\n')
-    t.write('dirnames:'+repr(dirs)+'\n')
+    t.write('dir names:'+repr(dirs)+'\n')
     t.write('files:'+repr(files)+'\n')
     print(repr(root))
     print(repr(dirs))
--- tkApp.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#-*-coding: utf-8-*-
-import tkinter 
-root = tkinter.Tk()
-w = tkinter.Label(root,text = 'fuck you!!')
-w.pack()
-root.mainloop()
