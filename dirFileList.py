71e66d2efa17244fa0239dfea0a33d8ed597f3df
sfzhang <andy.ledway3@gmail.com>
2016-11-21 23:57:19 -0500

    make adjustments

--- dirFileList.py
+++ dirFileList.py
@@ -3,7 +3,7 @@
 #2012.5.2 by  sfzhang
 import os
 list = os.listdir('E:/测试环境/abnf')
-t = open('E:/测试环境/result.txt','w')
+t = open('E:/测试环境/result.txt', 'w')
 for eachline in list:
     t.write('QISRWaveformSearch ,'+'"'+'D:/abnf/'+eachline+'"'+',,'+'\n')
 t.close()
