a893f93ee5032b7acaf87f1fddd662c7e8547d69
nill-zhang <andy.ledway3@gmail.com>
2016-12-10 23:47:09 -0500

    add test code snippet

--- context_manager.py
+++ context_manager.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# by sfzhang @2016.12.10
+# by sfzhang 2016.12.10
 from contextlib import contextmanager
 import sys
 
@@ -21,7 +21,7 @@ class Mirror(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.write = self.original_write
         if exc_type is NameError:
-            print("can not find variable")
+            print("An NameError happened")
             return True
 
 
@@ -42,12 +42,27 @@ def mirror(hand_writing):
 
 
 def test_class_mirror():
-    pass
+    with Mirror("sfzhang is a good guy!!") as comment:
+        print("start printing....")
+        print(comment)
+        print("finished printing....")
+        raise NameError("Raising an NameError before we finish")
+    print(comment)
+
+    try:
+        with Mirror("sfzhang is also a skilled guy") as another_comment:
+            print("start printing....")
+            print(another_comment)
+            print("finished printing....")
+            raise IndexError("Raising an IndexError before we finish")
+    except IndexError:
+        print("An IndexError Propagated from with statement is caught")
+    print(another_comment)
 
 
 def test_func_mirror():
     pass
 
 if __name__ == "__main__":
-    pass
+    test_class_mirror()
 
