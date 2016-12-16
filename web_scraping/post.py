0e33e1c32601ce71a334317843ef7a765abc9d25
nill-zhang <andy.ledway3@gmail.com>
2016-12-15 23:05:53 -0500

    initial framework of the module

---
+++ web_scraping/post.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# by sfzhang 2016.12.15
+import requests
+import urllib.request
+import mechanize
+
+
+def urllib_get():
+    pass
+
+
+def urllib_post():
+    pass
+
+
+def requests_post():
+    pass
+
+
+def mechanize_post():
+    pass
+
+
+if __name__ == "main":
+    pass
