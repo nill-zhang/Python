afc269879cc89aa70d9abaca3235d70f92aa0d2b
nill-zhang <andy.ledway3@gmail.com>
2016-12-21 03:44:08 -0500

    fix  twitter login code

--- web_scraping/twitter.py
+++ web_scraping/twitter.py
@@ -1,29 +1,28 @@
 #/usr/bin/python
 #by sfzhang 2016.12.17
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import urllib.request
 import urllib.parse
 import bs4 as bs
 
 
+def login(driver, t_uname, t_pword):
+    """
+    Automatically login to twitter account
+    Note: selenium only find elements that are descendants of
+          the current element, not siblings or siblings's descendants
+    """
 
-def get_api(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    #element = driver.find_element_by_link_text("Create New App")
-    #element = driver.find_element_by_class_name("btn btn")
-    element = driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
-    #element.clear()
-    element.send_keys("181715190@qq.com")
-    element = driver.find_element_by_xpath("//input[@name='session[password]']")
-    #element.clear()
-    element.send_keys("ZSFFSzz131085")
-    element.send_keys(Keys.RETURN)
-    element.find_element_by_tag_name("button").click()
-
-    assert "alex" in driver.title
-    print(driver.page_source)
+    boxes_parent = driver.find_element_by_tag_name("fieldset")
+    boxes = boxes_parent.find_elements_by_tag_name("input")
+    boxes[0].clear()  # this will clear any texts(if any) already in the textbox
+    boxes[0].send_keys(t_uname)
+    boxes[1].clear()
+    boxes[1].send_keys(t_pword)
+    button_parent = driver.find_element_by_class_name("clearfix")
+    button = button_parent.find_element_by_tag_name("button")
+    button.click()
+    return driver
 
 
 def sign_in(url):
@@ -36,7 +35,34 @@ def sign_in(url):
     print(soup.head)
 
 
+def initiate_driver(url):
+    web_driver = webdriver.Chrome()
+    web_driver.get(url)
+    return web_driver
+
+
+def create_app(driver):
+    """Access the create app webpage trying to create a third-party app"""
+    sign_in_button = driver.find_element_by_link_text("Sign in")
+    sign_in_button.get_attribute("href")
+    sign_in_button.click()
+    a = login(driver, "181715190@qq.com", "ZSFFSzz131085")
+    create_newapp_button = a.find_element_by_link_text("Create New App")
+    create_newapp_button.click()
+    #todo
+
+
+def test_create_app(url):
+    chrome_driver = initiate_driver(url)
+    create_app(chrome_driver)
+
+
+def test_login(url):
+    chrome_driver = initiate_driver(url)
+    create_app(chrome_driver, "181715190@qq.com", "ZSFFSzz131085")
+
 if __name__ == "__main__":
     # sign_in("https://twitter.com/sessions")
-    get_api("https://twitter.com/login")
+    test_create_app("https://apps.twitter.com/")
+    test_login("https://twitter.com/login")
 
