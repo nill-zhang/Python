72dfeab9dd09390820d5b1b47fede4b89e7f4abd
nill-zhang <andy.ledway3@gmail.com>
2016-12-26 01:52:04 -0500

    add robots.txt site.xml handlers

---
+++ web_scraping/site.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# by sfzhang 2016.2.24
+import urllib.request
+from urllib.robotparser import RobotFileParser
+
+rp = RobotFileParser()
+rp.set_url()
+agent = "BadCrawler"
+
+
+def get_url(url, max_retries, user_agent):
+    pass
+
+
+def crawl_sitemap(url):
+    pass
+
+
+if __name__ == "__main__":
+    pass
