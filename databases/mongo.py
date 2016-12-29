9810aa053347261d36b38e514bb788c63e8b921b
nill-zhang <andy.ledway3@gmail.com>
2016-12-29 03:53:56 -0500

    fix mongo remote connection issues

--- databases/mongo.py
+++ databases/mongo.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 from flask import Flask
 from flask_pymongo import PyMongo
-import flask_pymongo
+from pymongo.errors import OperationFailure
+from pprint import pprint
 import sys
 app = Flask(__name__)
 app.config.update({
-    "MONGO_URI": "mongodb://192.168.65.222:27017/companies",
-    "MONGO_USERNAME": "sfzhang",
-    "MONGO_PASSWORD": "password"
+    #using default port 27017/tcp
+    "MONGO_URI": "mongodb://sfzhang:sfzhang@192.168.65.222/test"
 })
 
+# Alternative:
+# app.config.update({
+#     "MONGO_HOST": "192.168.65.222",
+#     "MONGO_DBNAME": "test",
+#     "MONGO_USERNAME": "sfzhang",
+#     "MONGO_PASSWORD": "sfzhang"
+# })
 mongo = PyMongo(app)
+# make sure execute code under an active application context
 with app.app_context():
     try:
-        mongo.db.companies.find_one()
-    except pymongo.errors.OperationFailure:
+        items = mongo.db.companies.find()
+        for item in items:
+            print(item["founded_year"])
+    except OperationFailure:
         sys.stdout.write("Can not connect to database!")
