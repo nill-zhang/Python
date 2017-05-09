2b1c4704a38601e16219d048c0cd3055eb954e7b
nill-zhang <andy.ledway3@gmail.com>
2017-05-09 00:54:06 -0400

    print --> print()

--- myPrograms/hash_set.py
+++ myPrograms/hash_set.py
@@ -7,22 +7,23 @@ def set_construct():
     set_b = set(i for i in set_a)
     set_c = set([1, 23, 2, 3, 4, 2, 3, 2])
     set_c.update((2, 23, 42, 2, 5, 22222, 0.23))
-    print '\n'.join((repr(i) for i in (set_a, set_b, set_c)))
+    print('\n'.join((repr(i) for i in (set_a, set_b, set_c))))
     try:
         set_c.add([2, 4])
     except TypeError:
-        print "Set element must be hashable,[2,4] is not!"
+        print("Set element must be hashable,[2,4] is not!")
     # return a set unifies all the sets
     set_d = {chr(i) for i in range(30, 40)}  # set comprehension
     return set_a | set_b | set_c | set_d
 
 
 def set_operations(myset):
-    print myset
-    print "intersection: %r" % repr(myset & set([i for i in xrange(3)]))
-    print "Union: %r" % repr(myset | set(['hey,there']))
-    print "Difference: %r" % repr(myset - myset)
-    print "Symmetric Difference: %r" % repr(myset ^ set([i for i in xrange(5, 15)]))
+
+    print(myset)
+    print("intersection: %r" % repr(myset & set([i for i in xrange(3)])))
+    print("Union: %r" % repr(myset | set(['hey,there'])))
+    print("Difference: %r" % repr(myset - myset))
+    print("Symmetric Difference: %r" % repr(myset ^ set([i for i in xrange(5, 15)])))
 
 
 if __name__ == "__main__":
