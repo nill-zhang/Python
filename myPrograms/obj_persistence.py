8b801ff7ea7a63b1f6f59f265986b67ee3ac1797
nill-zhang <andy.ledway3@gmail.com>
2017-05-09 00:41:33 -0400

    reorganize

--- myPrograms/obj_persistence.py
+++ myPrograms/obj_persistence.py
@@ -27,37 +27,45 @@ class BankAccount(object):
                               'Balance:'.ljust(10),
                               str(self.amount).ljust(20))
 
-if __name__ == "__main__":
+
+
+def BankAccount_test():
+
     my_pickle_account = BankAccount('451902', 200.00)
     my_pickle_account.deposit(3999)
     my_pickle_account.inquiry()
-    print my_pickle_account.__dict__
+    print(my_pickle_account.__dict__)
     with open("mybank_pickle.txt", "wb") as pickle_file:
         pickle.dump(my_pickle_account, pickle_file)
         pickle_file.close()
     with open("mybank_pickle.txt") as f:
-        print "Picklefile Content:"
+        print("Picklefile Content:")
         pprint.pprint(f.readlines(), indent=3)
         f.close()
     with open("mybank_pickle.txt", "rb") as my_account_file:
         my_account = pickle.load(my_account_file)
         my_account_file.close()
-    print "Restored account object from pickle: \n%s" % my_account.__dict__
+    print("Restored account object from pickle: \n%s" % my_account.__dict__)
     #########################################################################
     my_shelve_account = BankAccount('113332', 48000)
-    print my_shelve_account.__dict__
+    print(my_shelve_account.__dict__)
     shelve_file = shelve.open("mybank_shelve.txt")
     shelve_file['bank'] = my_shelve_account
     shelve_file.close()
 
     with open("mybank_shelve.txt") as f:
-        print "Shelvefile Content:"
+        print
+        "Shelvefile Content:"
         pprint.pprint(f.readlines(), indent=3)
         f.close()
 
     my_account_file = shelve.open("mybank_shelve.txt")
     shelve_account = my_account_file['bank']
-    print "restored account object from shelve: \n%s" % shelve_account.__dict__
+    print("restored account object from shelve: \n%s" % shelve_account.__dict__)
+
+if __name__ == "__main__":
+    BankAccount_test()
+
 
 
 
