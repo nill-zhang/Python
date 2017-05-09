ef1db11db74fe0da4856434e7119bde23161476f
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-09 00:37:59 -0400

    reformat

--- myPrograms/parseOption.py
+++ myPrograms/parseOption.py
@@ -5,24 +5,29 @@ import optparse
 import sys
 import os
 
-info = 'Usage: %s [Options]' % sys.argv[0]
-desc = 'A python script using optparse module'
-prog_name = 'whatever'
 
-parser = optparse.OptionParser(usage=info, description=desc, prog=prog_name)
+def parser():
 
-parser.add_option('-c', action='store', dest='text')
-parser.add_option('--content', action='store', dest='text')
-parser.add_option('-o', action='store', dest='outputfile')
-parser.add_option('--output', action='store', dest='outputfile')
-parser.add_option('-d', action='store_true', dest='debug')
-parser.add_option('--debug', action='store_true', dest='debug')
+    info = 'Usage: %s [Options]' % sys.argv[0]
+    desc = 'A python script using optparse module'
+    prog_name = 'whatever'
 
-parser.set_defaults(debug=False)
+    parser = optparse.OptionParser(usage=info, description=desc, prog=prog_name)
 
-if __name__ == "__main__":
+    parser.add_option('-c', action='store', dest='text')
+    parser.add_option('--content', action='store', dest='text')
+    parser.add_option('-o', action='store', dest='outputfile')
+    parser.add_option('--output', action='store', dest='outputfile')
+    parser.add_option('-d', action='store_true', dest='debug')
+    parser.add_option('--debug', action='store_true', dest='debug')
+
+    parser.set_defaults(debug=False)
+
+
+def parser_test():
+
     opt, args = parser.parse_args()
-    print 'opt: %s, args: %s' % (opt, args)
+    print('opt: %s, args: %s' % (opt, args))
     if all([opt.text, opt.outputfile]):
         outfile = opt.outputfile
         debug_flag = opt.debug
@@ -30,12 +35,15 @@ if __name__ == "__main__":
         with open(outfile, 'w') as f:
             f.write(text_to_write)
     else:
-        print 'Usage: %s' % os.path.basename(sys.argv[0])
-        print '-h/--help     help'
-        print '-c/--content= content to write to the target file'
-        print '-d/--debug    execute command in debug mode'
-        print '-o/--output=  target file to write'
+        print('Usage: %s' % os.path.basename(sys.argv[0]))
+        print('-h/--help     help')
+        print('-c/--content= content to write to the target file')
+        print('-d/--debug    execute command in debug mode')
+        print('-o/--output=  target file to write')
         raise SystemExit(1)
+
+if __name__ == "__main__":
+    parser_test()
 
 
 
