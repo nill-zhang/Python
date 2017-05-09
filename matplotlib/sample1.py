d54a0a67f8633a62e0d2525609b4386a172925a6
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-09 01:51:59 -0400

    restructure this directory

--- matplotlib/sample1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# by sfzhang 2017.1.17
-from matplotlib import pyplot as ppt
-import random
-x_data = [random.randint(0, 10) for i in range(10)]
-y_data = [random.randint(0, 10) for i in range(10)]
-ppt.plot(x_data, y_data)
-ppt.show()
