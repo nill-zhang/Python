704801dbca6f785d7851907d889ac9f6b507575d
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-09 00:04:17 -0400

    add test

--- myPrograms/class_sample.py
+++ myPrograms/class_sample.py
@@ -11,39 +11,42 @@ class RbcAccount(object):
         self.balance = balance
         # self.deposit = func  # instance attribute, which takes precedence over class attribute(Rbcaccount.deposit)
         RbcAccount.account_number += 1
-        print '%d accounts exist in our bank' % RbcAccount.account_number
+        print('%d accounts exist in our bank' % RbcAccount.account_number)
 
     def __del__(self):
-        print 'deleting instance'
+        print('deleting instance')
         # RbcAccount.account_number -= 1
 
     def withdraw(self, amount):
         self.balance -= amount
-        print 'current balance after withdraw: %d' % self.balance
+        print('current balance after withdraw: %d' % self.balance)
 
     def deposit(self, amount):
         self.balance += amount
-        print 'current balance after deposit: %d' % self.balance
+        print('current balance after deposit: %d' % self.balance)
 
     def inquiry(self):
         return 'name: %s, balance : %d' % (self.name, self.balance)
 
 
 def custom(num):
-    print "num : %d" % num
+    print("num : %d" % num)
 
 
-account1 = RbcAccount('sfzhang', random.randint, custom)
-account2 = RbcAccount('xlyang', random.uniform(2, 200), custom)
-account3 = RbcAccount('lyzhang', random.randint, custom)
-account4 = RbcAccount('jchu', random.randint, custom)
-account5 = RbcAccount('qshu', random.randint, custom)
+def RbcAccount_test():
+    account1 = RbcAccount('sfzhang', random.randint, custom)
+    account2 = RbcAccount('xlyang', random.uniform(2, 200), custom)
+    account3 = RbcAccount('lyzhang', random.randint, custom)
+    account4 = RbcAccount('jchu', random.randint, custom)
+    account5 = RbcAccount('qshu', random.randint, custom)
+    name_list = ['Shaofeng Zhang', 'Xuelian Yang', 'Linyu Zhang', 'Ji Chu', 'Qiang Shu']
+    # Instances are not generated when you define a generator, until you iterate the generator
+    instance_list = (RbcAccount(item, random.uniform(0, 10000), custom) for item in name_list)
+    for i in instance_list:
+        print(i.inquiry())
 
-name_list = ['Shaofeng Zhang', 'Xuelian Yang', 'Linyu Zhang', 'Ji Chu', 'Qiang Shu']
-# Instances are not generated when you define a generator, until you iterate the generator
-instance_list = (RbcAccount(item, random.uniform(0, 10000), custom) for item in name_list)
-for i in instance_list:
-    print i.inquiry()
+if __name__ == "__main"__:
+    RbcAccount_test()
     
 
 
