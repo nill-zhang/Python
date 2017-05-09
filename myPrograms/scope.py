dfd1223cc21bc699973812b48536f8f356cc9d63
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-05-09 00:32:18 -0400

    change print to comply with python 3.0

--- myPrograms/scope.py
+++ myPrograms/scope.py
@@ -4,25 +4,25 @@ from pprint import pprint
 
 info = "global scope"
 number = 3
-print info
+print(info)
 
 
 def first_level():
     info = "first_level info from first_level scope"
-    print info
+    print(info)
     global info
     info = "global info from first_level scope"
-    print info
+    print(info)
 
     def second_level():
         info = "second_level info from second_level scope"
-        print info
+        print(info)
         global info
         info = "global info from second_level scope"
-        print info
+        print(info)
         global number
         number = 5
-        print "global number is changed to %d" % number
+        print("global number is changed to %d" % number)
 
     second_level()
 
@@ -33,27 +33,27 @@ y = 0
 def fun1():
     y = 11
     z = x + 1
-    print "fun1: x is %d, y is %d" % (x, y)
+    print("fun1: x is %d, y is %d" % (x, y))
     pprint(fun1.__globals__)
-    print repr(fun1.__closure__)
+    print(repr(fun1.__closure__))
 
     def fun2():
         y = 22
         z = 77
-        print "fun2: x is %d, y is %d" % (x, y)
+        print("fun2: x is %d, y is %d" % (x, y))
         pprint(fun2.__globals__)
-        print repr(fun2.__closure__)
+        print(repr(fun2.__closure__))
 
         def fun3():
             y = 33
-            print "fun3: x is %d, y is %d, z is %d" % (x, y, z)
+            print("fun3: x is %d, y is %d, z is %d" % (x, y, z))
             pprint(fun3.__globals__)
-            print repr(fun3.__closure__)
+            print(repr(fun3.__closure__))
         fun3()
     fun2()
 
 
 if __name__ == "__main__":
     first_level()
-    print info  # global info should be changed after calling first_level()
+    print(info)  # global info should be changed after calling first_level()
     fun1()
